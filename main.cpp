8cf149ceafa6158fad10478a6e054fca7be47f61
mark <82868532+marksmart15@users.noreply.github.com>
2025-04-08 13:54:55 -0700

    Update main.cpp to not have hard coded directory

--- main.cpp
+++ main.cpp
@@ -12,11 +12,9 @@ void ProcessAndSave(PPM original, const char* filterName, const string& outputFi
 }
 
 int main() {
-    string inputFilename = "/Users/camer/Documents/winter-douglas-25/computer-graphics/project/repo/Shahriar.ppm";
-    string outputPath = "/Users/camer/Documents/winter-douglas-25/computer-graphics/project/repo/outputImages/";
-    ifstream inputFile(inputFilename);
+    ifstream inputFile("input.ppm");
     if (!inputFile) {
-        cerr << "Failed to open input file: " << inputFilename << endl;
+        cerr << "Failed to open input file: input.ppm" << endl;
         return 1;
     }
 
@@ -24,25 +22,29 @@ int main() {
     inputFile.close();
 
     // Filters
-    ProcessAndSave(baseImage, "grayscale", outputPath + "output_grayscale.ppm");
-    ProcessAndSave(baseImage, "emboss",  outputPath +"output_emboss.ppm");
-    ProcessAndSave(baseImage, "edge",  outputPath +"output_edge.ppm");
-    ProcessAndSave(baseImage, "blur",  outputPath +"output_blur.ppm");
-    ProcessAndSave(baseImage, "invert",  outputPath +"output_invert.ppm");
-    ProcessAndSave(baseImage, "sharpen",  outputPath +"output_sharpen.ppm");
+    ProcessAndSave(baseImage, "grayscale", "output_grayscale.ppm");
+    ProcessAndSave(baseImage, "emboss", "output_emboss.ppm");
+    ProcessAndSave(baseImage, "edge", "output_edge.ppm");
+    ProcessAndSave(baseImage, "blur", "output_blur.ppm");
+    ProcessAndSave(baseImage, "invert", "output_invert.ppm");
+    ProcessAndSave(baseImage, "sharpen", "output_sharpen.ppm");
 
     // Effects
     PPM scaled = baseImage;
     Graphics::ScaleImage(scaled, 0.5);
     scaled.SaveImageToFile("output_scaled.ppm");
+    cout << "Saved: " << "output_scaled.ppm" << endl;
 
     PPM rotated = baseImage;
     Graphics::RotateImage(rotated, 90);
     rotated.SaveImageToFile("output_rotated.ppm");
+    cout << "Saved: " << "output_rotated.ppm" << endl;
 
     PPM translated = baseImage;
-    Graphics::TranslateImage(translated, 10, 10);
+    Graphics::TranslateImage(translated, 100, 100);
     translated.SaveImageToFile("output_translated.ppm");
+    cout << "Saved: " << "output_translated.ppm" << endl;
+
 
     cout << "All operations completed." << endl;
     return 0;
